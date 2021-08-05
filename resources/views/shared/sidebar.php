0a929429f3ed87ea3f3491cabed704aa892bdd80
Daniel BG <daniel@bettrads.com>
2021-08-04 21:57:10 -0300

    ft/barry

--- resources/views/shared/sidebar.php
+++ resources/views/shared/sidebar.php
@@ -4,7 +4,7 @@
 
         Tip 2: you can also add an image using data-image tag
     -->
-      <div class="logo"><a href="http://www.creative-tim.com" class="simple-text logo-normal">
+      <div class="logo"><a href="/" class="simple-text logo-normal">
           <img src="./assets/img/logo.jpeg" alt="" title="TSD" width="80">
         </a></div>
       <div class="sidebar-wrapper">
