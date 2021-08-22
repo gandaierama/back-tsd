59b6218c8aa1067f057d4ffa8f104e35d39a768a
Daniel Batista Galvão <danielbatistagalvao@hotmail.com>
2021-08-22 00:40:31 -0300

    Update header.php

--- resources/views/shared/header.php
+++ resources/views/shared/header.php
@@ -17,5 +17,6 @@
   <link href="./assets/css/material-dashboard.css?v=2.1.2" rel="stylesheet" />
   <!-- CSS Just for demo purpose, don't include it in your project -->
   <link href="./assets/demo/demo.css" rel="stylesheet" />
+  <script src="./assets/js/core/jquery.min.js"></script>
 </head>
 <body>
