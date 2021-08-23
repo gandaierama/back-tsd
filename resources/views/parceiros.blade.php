09fda71a8e943f259ab3ff559d8aaa97f915c469
Daniel Batista Galvão <danielbatistagalvao@hotmail.com>
2021-08-23 19:50:16 -0300

    Update parceiros.blade.php

--- resources/views/parceiros.blade.php
+++ resources/views/parceiros.blade.php
@@ -177,6 +177,12 @@
                           <input  name="complemento" type="text" class="form-control">
                         </div>
                       </div>
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Bairro</label>
+                          <input  name="bairro" type="text" class="form-control">
+                        </div>
+                      </div>
                     </div>
                     <div class="row">
                       <div class="col-md-4">
